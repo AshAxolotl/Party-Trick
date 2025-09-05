1c78e117891404a6f2cced04f2cbcb4cc4439322
AshAxolotl <87580179+AshAxolotl@users.noreply.github.com>
2025-09-05 22:04:24 +0200

    init

---
+++ src/client/java/com/ashaxolotl/partytrick/PartyTrickClient.java
@@ -0,0 +1,10 @@
+package com.ashaxolotl.partytrick;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PartyTrickClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
