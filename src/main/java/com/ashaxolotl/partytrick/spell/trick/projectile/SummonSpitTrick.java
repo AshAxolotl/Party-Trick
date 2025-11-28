aa6d021fa573370e9761556eb10c32fbee5894d7
AshAxolotl <87580179+AshAxolotl@users.noreply.github.com>
2025-11-28 15:26:33 +0100

    fix llama ploy on servers WHY IS IT LIKE THAT

--- src/main/java/com/ashaxolotl/partytrick/spell/trick/projectile/SummonSpitTrick.java
+++ src/main/java/com/ashaxolotl/partytrick/spell/trick/projectile/SummonSpitTrick.java
@@ -10,9 +10,12 @@ import dev.enjarai.trickster.spell.fragment.FragmentType;
 import dev.enjarai.trickster.spell.fragment.VectorFragment;
 import dev.enjarai.trickster.spell.trick.Trick;
 import dev.enjarai.trickster.spell.type.Signature;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.ShulkerEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.util.math.Vec3d;
 
 
@@ -25,14 +28,7 @@ public class SummonSpitTrick extends Trick<SummonSpitTrick> {
 
     public EntityFragment fromCaster(SpellContext ctx, VectorFragment pos)  throws BlunderException {
         var caster = ctx.source().getCaster().orElseThrow(() -> new NoPlayerBlunder(this));
-        ctx.useMana(this, cost(ctx.source().getPos().distance(pos.vector())));
-
-        var world = ctx.source().getWorld();
-        var projectile = EntityType.LLAMA_SPIT.create(world);
-        projectile.setOwner(caster);
-        projectile.setPos(pos.x(), pos.y(), pos.z());
-        world.spawnEntity(projectile);
-        return EntityFragment.from(projectile);
+        return spell(ctx, pos, caster);
     }
 
     public EntityFragment fromEntity(SpellContext ctx, VectorFragment pos, EntityFragment entityFragment) throws BlunderException {
@@ -40,17 +36,24 @@ public class SummonSpitTrick extends Trick<SummonSpitTrick> {
         if (!(entity instanceof LivingEntity) || entity instanceof PlayerEntity || entity.getType().isIn(Tags.SPIT_BLACKLIST)) {
             throw new EntityInvalidBlunder(this);
         }
+        return spell(ctx, pos, entity);
+    }
+
+    private EntityFragment spell(SpellContext ctx, VectorFragment pos, Entity entity) throws BlunderException {
         ctx.useMana(this, cost(entity.getPos().distanceTo(new Vec3d(pos.x(), pos.y(), pos.z()))));
 
         var world = ctx.source().getWorld();
-        var projectile = EntityType.LLAMA_SPIT.create(world);
+        ProjectileEntity projectile;
+        if (entity instanceof ShulkerEntity) {
+            projectile = EntityType.SHULKER_BULLET.create(world);
+        } else {
+            projectile = EntityType.LLAMA_SPIT.create(world);
+        }
         projectile.setOwner(entity);
         projectile.setPos(pos.x(), pos.y(), pos.z());
         world.spawnEntity(projectile);
+        projectile.setPos(pos.x(), pos.y(), pos.z()); // yes you need to do this again else spit will instantly disappear when 0, 0, 0 has a block on a server. Minecraft WTF
         return EntityFragment.from(projectile);
-
-
-
     }
 
     private float cost(double dist) {
